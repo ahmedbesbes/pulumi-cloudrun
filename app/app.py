5d097de9ee84c0b295551929ca4495321f247467
Ahmed Besbes <ahmed.besbes@hotmail.com>
2024-03-17 23:41:38 +0100

    Add test

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ st.header("Hello from Cloud Run !")
 
 show_balloons = st.button("Show balloons!")
 
+st.write("This change has been deployed via a CICD powered by Cloudbuild")
+
 if show_balloons:
     st.balloons()
     st.write("🎈")
